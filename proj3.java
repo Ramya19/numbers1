3b865c7d2e7f5a0c5b2dea23b2fd81f5e7288e95
Ramya19 <ramyamohanit@gmail.com>
2016-07-16 15:33:40 +0530

    Add files via upload

---
+++ proj3.java
@@ -0,0 +1,13 @@
+{
+       
+        Scanner sc=new Scanner (System.in);
+         char a=sc.next().charAt(0);
+        
+        
+        // TODO code application logic here
+        if(a=='a'||a=='i'||a=='o'||a=='u'||a=='e')
+            System.out.println("vowel");
+        else
+            System.out.println("consonant");
+       
+    }
