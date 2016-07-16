9393bec08a5a1eebbba87c9a1a0957cff6e2b28f
Ramya19 <ramyamohanit@gmail.com>
2016-07-16 17:31:13 +0530

    Add files via upload

---
+++ Beg1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beg1;
+
+/**
+ *
+ * @author Student
+ */
+public class Beg1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //String max;
+        //int i;
+        // TODO code application logic here
+        int fact=1;
+        int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println("the factorial of"+n+"is"+fact);
+ 
+        
+       
+    }
+}
